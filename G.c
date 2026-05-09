6073a9ae2d72c630e98ee82d729d910243241ed8
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-05-09 16:51:16 +0600

    Add files via upload

---
+++ G.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+
+   int n;
+   scanf("%d",&n);
+   while(n--){
+int a;
+scanf("%d",&a);
+
+int array[a];
+for(int i = 0;i<a;i++){
+scanf("%d",&array[i]);
+}
+int count=0;
+for(int i =0;i<a;i++){
+    if(array[i]%2==0){
+        
+    }
+}
+
+
+
+
+   }
+   
+return 0;
+}
