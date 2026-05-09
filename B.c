1d0320efb9f0905a1312ab77e7023c5743692586
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-05-09 16:49:57 +0600

    Add files via upload

---
+++ B.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+
+    int n;
+    scanf("%d",&n);
+while(n--){
+
+int a,x,points,count =0;
+scanf("%d%d",&x,&a);
+x = x/10;
+ 
+
+
+points = x*a;
+printf("%d\n",points);
+
+
+
+
+
+}
+
+return 0;
+}
