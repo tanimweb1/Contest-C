ac0d8df348e40aebd7cc2eda7a4dd9ad98250809
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-08-28 23:40:14 +0600

    Add files via upload

---
+++ Untitled5.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+
+int a,b,c,d,e;
+scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+
+if(a==0){
+    printf("1\n");
+}
+
+
+else if(b==0){
+    printf("2\n");
+}
+
+
+else if(c==0){
+   printf("3\n");
+}
+
+
+else if(d==0)
+    {printf("4\n");}
+
+else if(e==0){
+    printf("5\n");
+}
+
+
+
+return 0;
+}
+
+
+
