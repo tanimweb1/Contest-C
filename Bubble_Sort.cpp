86b166bdbc6079a5d0cd741978989acfac31765c
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-09 00:46:11 +0600

    Add files via upload

---
+++ Bubble_Sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[100], n, pass = 0;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - i - 1; j++)
+        {
+            if(arr[j] < arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+        }
+
+        pass++;
+    }
+
+    cout << "Sorted array: ";
+
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    cout << endl;
+    cout << "Number of passes: " << pass << endl;
+
+    return 0;
+}
